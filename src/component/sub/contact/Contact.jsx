a417e92b2ad3653946b7dc107c1651c6096233db
Minminjamin <0429el@gmail.com>
2023-09-26 15:16:48 +0900

    feature : Contact 컴포넌트

---
+++ src/component/sub/contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../common/layout/Layout";
+
+const Contact = () => {
+  return (
+    <Layout title={"Contact"}>
+      <p>Contact 페이지 입니다.</p>
+    </Layout>
+  );
+};
+
+export default Contact;
