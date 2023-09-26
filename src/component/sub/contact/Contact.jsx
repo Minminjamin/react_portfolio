0855c836ab4a1ee89c39eb71e93c4bc50f7d56ce
Minminjamin <0429el@gmail.com>
2023-09-26 16:09:58 +0900

    style : figure의 스타일 가지고 놀기

--- src/component/sub/contact/Contact.jsx
+++ src/component/sub/contact/Contact.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Layout from "../../common/layout/Layout";
+import styles from "../contact/Contact.module.scss";
 
 const Contact = () => {
   return (
-    <Layout title={"Contact"}>
+    <Layout title={"Contact"} styleName={styles.contact}>
       <p>Contact 페이지 입니다.</p>
     </Layout>
   );
--- src/component/sub/department/Department.jsx
+++ src/component/sub/department/Department.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import clsx from "clsx";
 import styles from "../department/Department.module.scss";
 import Layout from "../../common/layout/Layout";
 
 const Department = () => {
   return (
-    <Layout title={"Department"}>
+    <Layout title={"Department"} styleName={styles.content}>
       <p>department 페이지 입니다.</p>
     </Layout>
   );
