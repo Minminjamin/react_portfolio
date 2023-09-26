41f8aa421f4f277aff62cd87b3f6112bda7b6fcc
Minminjamin <0429el@gmail.com>
2023-09-26 16:02:11 +0900

    feature : layout 컴포넌트가 스타일을 받을 수 있도록 수정

--- src/component/common/layout/Layout.jsx
+++ src/component/common/layout/Layout.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import styles from "../layout/Layout.module.scss";
 import clsx from "clsx";
 
-const Layout = ({ title, children }) => {
+const Layout = ({ title, children, styleName }) => {
   return (
-    <section className={clsx(styles.layout)}>
+    <section className={clsx(styles.layout, styleName)}>
       <figure></figure>
 
       <div className={clsx(styles.content)}>
