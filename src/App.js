ee738f8d2984cf431dfc8719835cc4cb5eaa7abc
Minminjamin <0429el@gmail.com>
2023-09-26 14:43:40 +0900

    feature : youtube 컴포넌트

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Header from "./component/common/header/Header";
 import "./styles/global.scss";
 import Department from "./component/sub/department/Department";
+import Youtube from "./component/sub/youtube/Youtube";
 
 function App() {
   return (
     <>
       <Header />
-      <Department />
+      <Youtube />
+      {/* <Department /> */}
     </>
   );
 }
---
+++ src/component/sub/youtube/Youtube.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import clsx from "clsx";
+// import styles from "../Youtube/Youtube.module.scss";
+import Layout from "../../common/layout/Layout";
+
+const Youtube = () => {
+  return (
+    <Layout title={"Youtube"}>
+      <p>Youtube 페이지 입니다.</p>
+    </Layout>
+  );
+};
+
+export default Youtube;
