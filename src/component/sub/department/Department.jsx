27ff21b78e60c5a7be1ac941e91e402c88a04fdb
Minminjamin <0429el@gmail.com>
2023-10-05 22:52:53 +0900

    style

--- src/component/sub/department/Department.jsx
+++ src/component/sub/department/Department.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styles from "../department/Department.module.scss";
 import Layout from "../../common/layout/Layout";
+import clsx from "clsx";
 
 const path = process.env.PUBLIC_URL;
 
@@ -8,6 +9,7 @@ const Department = () => {
   const [department, setDepartment] = useState([]);
   const [history, setHistory] = useState([]);
 
+  const [isHover, setIsHover] = useState(false);
   useEffect(() => {
     fetch(`${path}/DB/department.json`)
       .then((data) => data.json())
@@ -28,15 +30,17 @@ const Department = () => {
         {history.map((item, index) => (
           <article key={index}>
             {/* 일단 json의 key에 접근(2016, 2018) */}
-            <h2>{Object.keys(item)}</h2>
 
+            <div className={styles.inner}>
+              <h2>{Object.keys(item)}</h2>
+              <ul>
+                {Object.values(item).map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            </div>
             {/* 그리고 나서 json 키의 value에 접근해서 value의 값을 하나하나 뽑아냄 */}
             {/* Object.values(item)이라는 것은 item에 담긴 값을 추출하는 것 */}
-            <ul>
-              {Object.values(item).map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
           </article>
         ))}
       </div>
@@ -46,10 +50,15 @@ const Department = () => {
           <article key={index}>
             <div className={styles.pic}>
               <img src={`${path}/img/${member.pic}`} alt={member.name} />
+              <div className={styles.imgInnerText}>
+                <h2>{member.name}</h2>
+                <span>{member.position}</span>
+              </div>
             </div>
-
-            <h2>{member.name}</h2>
-            <span>{member.position}</span>
+            {/* 
+            <div className={styles.text}>
+              <span>{member.position}</span>
+            </div> */}
           </article>
         ))}
       </div>
