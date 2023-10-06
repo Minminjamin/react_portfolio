b6ef80b1c39cf478251baf32af0e5e2257c68749
Minminjamin <0429el@gmail.com>
2023-10-07 00:08:32 +0900

    feature : 카카오 지도 출력

--- src/component/sub/contact/Contact.jsx
+++ src/component/sub/contact/Contact.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import Layout from "../../common/layout/Layout";
 import styles from "../contact/Contact.scss";
 
 const Contact = () => {
+  const map = useRef(null);
+
+  const { kakao } = window;
+
+  const mapOption = {
+    center: new kakao.maps.LatLng(37.584943947008455, 126.8857970238249), // 지도의 중심좌표
+    level: 1, // 지도의 확대 레벨
+  };
+  const markerPosition = mapOption.center;
+
+  const marker = new kakao.maps.Marker({
+    position: markerPosition,
+  });
+  useEffect(() => {
+    const instance = new kakao.maps.Map(map.current, mapOption);
+    marker.setMap(instance);
+  }, []);
+
   return (
     <Layout title={"Contact"} styleName={styles.contact}>
-      <p>Contact 페이지 입니다.</p>
+      <div className="map" ref={map}></div>
     </Layout>
   );
 };
