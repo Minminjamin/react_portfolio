e21ec6b10dcbca58fd0a7dc23ba3142bdf3750d8
Minminjamin <0429el@gmail.com>
2023-10-07 00:12:52 +0900

    feature : 지도의 마커 이미지 변경& 교통 정보 표시

--- src/component/sub/contact/Contact.jsx
+++ src/component/sub/contact/Contact.jsx
@@ -3,27 +3,55 @@ import Layout from "../../common/layout/Layout";
 import styles from "../contact/Contact.scss";
 
 const Contact = () => {
+  const instance = useRef(null);
   const map = useRef(null);
 
   const { kakao } = window;
 
-  const mapOption = {
-    center: new kakao.maps.LatLng(37.584943947008455, 126.8857970238249), // 지도의 중심좌표
-    level: 1, // 지도의 확대 레벨
+  const info = {
+    latlng: new kakao.maps.LatLng(37.584943947008455, 126.8857970238249),
+    imageSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
+    imageSize: new kakao.maps.Size(232, 99),
+    imageOption: { offset: new kakao.maps.Point(116, 99) },
   };
-  const markerPosition = mapOption.center;
 
   const marker = new kakao.maps.Marker({
-    position: markerPosition,
+    position: info.latlng,
+    image: new kakao.maps.MarkerImage(
+      info.imageSrc,
+      info.imageSize,
+      info.imagePos
+    ),
   });
+
   useEffect(() => {
-    const instance = new kakao.maps.Map(map.current, mapOption);
-    marker.setMap(instance);
+    instance.current = new kakao.maps.Map(map.current, {
+      center: info.latlng,
+      level: 1,
+    });
+    marker.setMap(instance.current);
   }, []);
 
   return (
     <Layout title={"Contact"} styleName={styles.contact}>
-      <div className="map" ref={map}></div>
+      <div className="map" ref={map}>
+        <button
+          onClick={() => {
+            instance.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+          }}
+        >
+          주변 교통정보 보기
+        </button>
+        <button
+          onClick={() =>
+            instance.current.removeOverlayMapTypeId(
+              kakao.maps.MapTypeId.TRAFFIC
+            )
+          }
+        >
+          주변 교통정보 끄기
+        </button>
+      </div>
     </Layout>
   );
 };
