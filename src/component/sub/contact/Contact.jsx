890c4331fa67d63d62ff19eb04b3a2e09fb0742c
Minminjamin <0429el@gmail.com>
2023-10-07 14:48:35 +0900

    refact : 환경 변수를 사용해 key값 등록

--- src/component/sub/contact/Contact.jsx
+++ src/component/sub/contact/Contact.jsx
@@ -85,10 +85,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "YOUR_SERVICE_ID",
-        "YOUR_TEMPLATE_ID",
+        `${process.env.REACT_APP_EMAILJS_SERVICE_ID}`,
+        `${process.env.REACT_APP_EMAILJS_TEMPLATE_ID}`,
         form.current,
-        "YOUR_PUBLIC_KEY"
+        `${process.env.REACT_APP_EMAILJS_PUBLIC_KEY}`
       )
       .then(
         (result) => {
