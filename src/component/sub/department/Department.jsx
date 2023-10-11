7a423b5433e745db94ab008181eaa2d857f4aa40
Minminjamin <0429el@gmail.com>
2023-10-11 12:53:35 +0900

    style : deaartment 스타일

--- src/component/sub/department/Department.jsx
+++ src/component/sub/department/Department.jsx
@@ -26,43 +26,56 @@ const Department = () => {
 
   return (
     <Layout title={"Department"} styleName={styles.department}>
-      <h2 className={styles.title}>Our History</h2>
-      <div className={styles.historyBox}>
-        {[...history].reverse().map((item, index) => (
+      <h2>Team</h2>
+      <div className={styles.line}></div>
+      <div className={styles.memberBox}>
+        {department.map((member, index) => (
           <article key={index}>
-            {/* 일단 json의 key에 접근(2016, 2018) */}
-
-            <div className={styles.inner}>
-              <h3>{Object.keys(item)}</h3>
-              <ul>
-                {Object.values(item).map((item, index) => (
-                  <li key={index}>{item}</li>
-                ))}
-              </ul>
+            <h3>{member.name}</h3>
+            <div className={styles.line}></div>
+            <div className={styles.pic}>
+              <img src={`${path}/img/${member.pic}`} alt={member.name} />
             </div>
-            {/* 그리고 나서 json 키의 value에 접근해서 value의 값을 하나하나 뽑아냄 */}
-            {/* Object.values(item)이라는 것은 item에 담긴 값을 추출하는 것 */}
+            <p>{member.position}</p>
+            <p>
+              Email: <a href={`mailto:${member.mail}`}>{member.mail}</a>
+            </p>
+            <p>Phone : {member.phone}</p>
           </article>
         ))}
       </div>
 
-      <h2>Members</h2>
-      <div className={styles.memberBox}>
-        {department.map((member, index) => (
-          <article key={index}>
-            <div className={styles.pic}>
-              <img src={`${path}/img/${member.pic}`} alt={member.name} />
-              <div className={styles.imgInnerText}>
-                <h3>{member.name}</h3>
-                <span>{member.position}</span>
-              </div>
-            </div>
-            {/* 
-            <div className={styles.text}>
-              <span>{member.position}</span>
-            </div> */}
-          </article>
-        ))}
+      <h2>About</h2>
+      <div className={styles.line}></div>
+      <div className={styles.aboutBox}>
+        <div className={styles.pic}>
+          <img src={`${path}/img/department01.jpg`} alt="bird" />
+        </div>
+        <div className={styles.text}>
+          <h3>Philosophy</h3>
+          {/* <div className={styles.innerText}> */}
+          <h4>
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+            tempore quibusdam odio! Eius esse adipisci possimus voluptatem eos
+            laboriosam doloribus!"
+          </h4>
+          {/* </div> */}
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed
+            quibusdam cupiditate et eveniet corporis, tempora adipisci ducimus
+            perferendis qui obcaecati quas modi totam blanditiis, earum ut
+            accusantium? Sunt totam ab modi expedita rem iusto quaerat ipsa
+            architecto. Saepe, quibusdam molestias quidem, nihil voluptatibus
+            culpa dolorem quo quae tenetur commodi, soluta nesciunt optio
+            debitis aut reprehenderit nisi blanditiis sapiente esse veniam!
+            Quod, ullam. Nam accusantium quisquam corporis sed aliquid dolore
+            quia officiis aliquam consequuntur quibusdam similique ut, culpa
+            reiciendis. Quos, consequatur. Quod quaerat nostrum voluptatem fuga
+            voluptatibus commodi asperiores dignissimos voluptatum, blanditiis
+            itaque animi nulla harum et sit cumque aperiam culpa?
+          </p>
+        </div>
       </div>
     </Layout>
   );
