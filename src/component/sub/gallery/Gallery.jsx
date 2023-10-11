570d19ec30cd13cfebdd2d4951c5d885c6ad9394
Minminjamin <0429el@gmail.com>
2023-10-11 09:27:22 +0900

    feature & refact : modal, 코드 개선

--- src/component/sub/gallery/Gallery.jsx
+++ src/component/sub/gallery/Gallery.jsx
@@ -3,12 +3,15 @@ import Layout from "../../common/layout/Layout";
 import "./Gallery.scss";
 import { useState, useEffect, useRef } from "react";
 import Masonry from "react-masonry-component";
+import Modal from "../../common/modal/Modal";
 
 const Gallery = () => {
   const [pics, setPics] = useState([]);
   const [loader, setLoader] = useState(true);
   const [fix, setFix] = useState(false);
   const [isUser, setIsUser] = useState(true);
+  const [activeUrl, setActiveUrl] = useState("");
+  const [open, setOpen] = useState(false);
 
   const frame = useRef(null);
   const search = useRef(null);
@@ -61,119 +64,143 @@ const Gallery = () => {
         }
       };
     });
-    // if()
-    // console.log(json);
+  };
+
+  const onHanldeSubmit = (e) => {
+    e.preventDefault();
+    setIsUser(false);
+
+    const btns = btnSet.current.querySelectorAll("button");
+    btns.forEach((btn) => btn.classList.remove("on"));
+
+    if (search.current.value.trim() === "") {
+      return alert("검색어를 입력하세요");
+    }
+    fetchData({ type: "search", tags: search.current.value });
+    search.current.value = "";
   };
 
   useEffect(() => {
+    console.log(fetchData({ type: "user", id: myId }));
     // fetchData({ type: "search", tags: "landscape" });
     fetchData({ type: "user", id: myId });
     // fetchData({ type: "user" });
   }, []);
 
   return (
-    <Layout title={"Gallery"}>
-      <div className="searchBox">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            setIsUser(false);
+    <>
+      <Layout title={"Gallery"}>
+        <div className="searchBox">
+          <form
+            onSubmit={(e) => {
+              onHanldeSubmit();
+            }}
+          >
+            <input
+              ref={search}
+              type="text"
+              placeholder="검색어를 입력하세요."
+            />
+            <button>검색</button>
+          </form>
+        </div>
 
-            if (search.current.value.trim() === "") {
-              return alert("검색어를 입력하세요");
-            }
-            fetchData({ type: "search", tags: search.current.value });
-            search.current.value = "";
-          }}
-        >
-          <input ref={search} type="text" placeholder="검색어를 입력하세요." />
-          <button>검색</button>
-        </form>
-      </div>
+        <div className="btnSet" ref={btnSet}>
+          <button
+            className="on"
+            onClick={(e) => {
+              setIsUser(true);
+              if (e.target.classList.contains("on")) return;
+              const btns = btnSet.current.querySelectorAll("button");
+              btns.forEach((btn) => btn.classList.remove("on"));
+              e.target.classList.add("on");
+              fetchData({ type: "user", id: myId });
+            }}
+          >
+            나의 갤러리 호출
+          </button>
+          <button
+            onClick={(e) => {
+              setIsUser(false);
+              if (e.target.classList.contains("on")) return;
+              const btns = btnSet.current.querySelectorAll("button");
+              btns.forEach((btn) => btn.classList.remove("on"));
+              e.target.classList.add("on");
 
-      <div className="btnSet" ref={btnSet}>
-        <button
-          className="on"
-          onClick={(e) => {
-            setIsUser(true);
-            const btns = btnSet.current.querySelectorAll("button");
-            btns.forEach((btn) => btn.classList.remove("on"));
-            e.target.classList.add("on");
-            fetchData({ type: "user", id: myId });
-          }}
-        >
-          나의 갤러리 호출
-        </button>
-        <button
-          onClick={(e) => {
-            setIsUser(false);
-            const btns = btnSet.current.querySelectorAll("button");
-            btns.forEach((btn) => btn.classList.remove("on"));
-            e.target.classList.add("on");
+              fetchData({ type: "interest" });
+            }}
+          >
+            interest 갤러리
+          </button>
+        </div>
 
-            fetchData({ type: "interest" });
-          }}
-        >
-          interest 갤러리
-        </button>
-      </div>
-
-      {loader && (
-        <img
-          className="loading"
-          src={`${process.env.PUBLIC_URL}/img/loading.gif`}
-          alt="loading"
-        />
-      )}
-      <div className="picFrame" ref={frame}>
-        <Masonry
-          elementType={"div"}
-          options={{ transitionDuration: "0.5s" }}
-          disableImagesLoaded={false}
-          updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
-        >
-          {pics.map((item, index) => (
-            <article key={index}>
-              <div className="inner">
-                <img
-                  className="pic"
-                  src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
-                  alt={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg`}
-                />
-                <h2>{item.title}</h2>
-
-                <div className="profile">
+        {loader && (
+          <img
+            className="loading"
+            src={`${process.env.PUBLIC_URL}/img/loading.gif`}
+            alt="loading"
+          />
+        )}
+        <div className="picFrame" ref={frame}>
+          <Masonry
+            elementType={"div"}
+            options={{ transitionDuration: "0.5s" }}
+            disableImagesLoaded={false}
+            updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+          >
+            {pics.map((item, index) => (
+              <article key={index}>
+                <div className="inner">
                   <img
-                    src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}ggr.jpg`}
-                    alt={item.owner}
-                    onError={(e) => {
-                      console.log(e.target);
-                      setFix(true);
-                      e.target.setAttribute(
-                        "src",
-                        "https://www.flickr.com/images/buddyicon.gif"
-                      );
+                    className="pic"
+                    src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
+                    alt={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg`}
+                    onClick={(e) => {
+                      setActiveUrl(e.target.getAttribute("alt"));
+                      setOpen(true);
                     }}
                   />
-                  <span
-                    onClick={() => {
-                      //사용자 아이디 클릭시 현재 출력되는 갤러리가 User 타입 갤러리면 이벤트 호출 방지
-                      if (isUser) return;
+                  <h2>{item.title}</h2>
 
-                      //fetchData가 실행이 되면 다시 User type갤러리로 변경되므로 다시 IsUser값을 true로 변경
-                      fetchData({ type: "user", id: item.owner });
-                      setIsUser(true);
-                    }}
-                  >
-                    {item.owner}
-                  </span>
+                  <div className="profile">
+                    <img
+                      src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
+                      alt={item.owner}
+                      onError={(e) => {
+                        console.log(e.target);
+                        setFix(true);
+                        e.target.setAttribute(
+                          "src",
+                          "https://www.flickr.com/images/buddyicon.gif"
+                        );
+                      }}
+                    />
+                    <span
+                      onClick={() => {
+                        //사용자 아이디 클릭시 현재 출력되는 갤러리가 User 타입 갤러리면 이벤트 호출 방지
+                        if (isUser) return;
+
+                        //fetchData가 실행이 되면 다시 User type갤러리로 변경되므로 다시 IsUser값을 true로 변경
+                        fetchData({ type: "user", id: item.owner });
+                        setIsUser(true);
+                      }}
+                    >
+                      {item.owner}
+                    </span>
+                  </div>
                 </div>
-              </div>
-            </article>
-          ))}
-        </Masonry>
-      </div>
-    </Layout>
+              </article>
+            ))}
+          </Masonry>
+        </div>
+      </Layout>
+
+      {open && (
+        <Modal>
+          <img src={activeUrl} alt="img" />
+        </Modal>
+      )}
+    </>
   );
 };
 
