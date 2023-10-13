2eb014dc01750807bd5913b92e795af225cfd7b2
Minminjamin <0429el@gmail.com>
2023-10-13 09:48:17 +0900

    feature : 기본 post(create)로직

--- src/component/sub/community/Community.jsx
+++ src/component/sub/community/Community.jsx
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Layout from "../../common/layout/Layout";
 import "../community/Community.scss";
 
 const Community = () => {
+  const refInput = useRef(null);
+  const refTextArea = useRef(null);
+
+  const [post, setPost] = useState([]);
+
+  const resetForm = () => {
+    refInput.current.value = "";
+    refTextArea.current.value = "";
+  };
+
+  const createPost = () => {
+    if (!refInput.current.value.trim() || !refTextArea.current.value.trim()) {
+      resetForm();
+      return alert("제목과 본문을 모두 입력하세요.");
+    }
+    setPost([
+      { title: refInput.current.value, content: refTextArea.current.value },
+      ...post,
+    ]);
+  };
+
   return (
     <Layout title={"Community"}>
-      <p>게싶판입니다.</p>
+      <div className="inputBox">
+        <input type="text" placeholder="제목을 입력하세요." ref={refInput} />
+        <br />
+        <textarea
+          ref={refTextArea}
+          cols="30"
+          rows="3"
+          placeholder="본문을 입력하세요."
+        ></textarea>
+
+        <nav className="btnSet">
+          <button onClick={resetForm}>Cancel</button>
+          <button onClick={createPost}>Write</button>
+        </nav>
+      </div>
+
+      <div className="showBox">
+        {post.map((item, idx) => (
+          <article key={idx}>
+            <h2>{item.title}</h2>
+            <p>{item.content}</p>
+          </article>
+        ))}
+      </div>
     </Layout>
   );
 };
