0e68d2b8e185027a1aecd2b655a9fc91867884c3
Minminjamin <0429el@gmail.com>
2023-10-13 15:39:33 +0900

    feature & style : news 컴포넌트에 게시글 출력, news 컴포넌트 스타일링

--- src/component/main/news/News.jsx
+++ src/component/main/news/News.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import "./News.scss";
 
 const News = () => {
+  const getLocalData = () => {
+    const data = localStorage.getItem("post");
+    return JSON.parse(data);
+  };
+  const [post] = useState(getLocalData());
+
   return (
     <section className="news">
       <h2>News</h2>
+      <div className="postWrap">
+        {post.map((item, idx) => {
+          if (idx >= 3) return null;
+          else
+            return (
+              <article key={idx}>
+                <h2>{item.title}</h2>
+                <span>{item.content}</span>
+              </article>
+            );
+        })}
+      </div>
     </section>
   );
 };
