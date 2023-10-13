1ee8d2367976706c73a6b331e502fa11f672f393
Minminjamin <0429el@gmail.com>
2023-10-13 16:18:10 +0900

    style : Info 컴포넌트 스타일링

--- src/component/main/info/Info.jsx
+++ src/component/main/info/Info.jsx
@@ -3,7 +3,14 @@ import "../info/Info.scss";
 const Info = () => {
   return (
     <section className="info">
-      <h2>Infomation</h2>
+      <div className="wrap">
+        <article>1</article>
+        <article>2</article>
+        <article>3</article>
+        <article>4</article>
+        <article>5</article>
+        <article>6</article>
+      </div>
     </section>
   );
 };
