24d32ec7e0b7d0ded087d9f0a1cbcf8ccaa4a456
Minminjamin <0429el@gmail.com>
2023-10-16 14:41:27 +0900

    feature : 관심사, 학력, 남길말 입력 기능

--- src/component/sub/members/Members.jsx
+++ src/component/sub/members/Members.jsx
@@ -10,6 +10,9 @@ const Members = () => {
     pw2: "",
     email: "",
     genders: false,
+    interests: false,
+    edu: "",
+    comments: "",
   };
 
   const [val, setVal] = useState(initval);
@@ -25,6 +28,16 @@ const Members = () => {
   const onHanldeRadio = (e) => {
     const { name, checked } = e.target;
     setVal({ ...val, [name]: checked });
+  };
+
+  const onHandleCheck = (e) => {
+    const { name } = e.target;
+    let isChecked = false;
+    const inputs = e.target.parentElement.querySelectorAll("input");
+
+    inputs.forEach((input) => input.checked && (isChecked = true));
+
+    setVal({ ...val, [name]: isChecked });
   };
 
   // 인수값으로 state를 전달받아서 각 데이터별로 인증처리 후 만약 인증 에러가 발생하면 해당 name 값으로 에러 문구를 생성해서 반환하는 함수
@@ -67,7 +80,19 @@ const Members = () => {
     }
 
     if (!value.genders) {
-      checkeErrs.genders = "성별을 하나 이상 체크해주세요.";
+      checkeErrs.genders = "성별은 필수 체크항목입니다.";
+    }
+
+    if (!value.interests) {
+      checkeErrs.interests = "관심사를 하나 이상 체크해주세요.";
+    }
+
+    if (!value.edu) {
+      checkeErrs.edu = "학력을 선택하세요.";
+    }
+
+    if (value.comments.length < 10) {
+      checkeErrs.comments = "남길 말은 10글자 이상 입력해주세요.";
     }
     return checkeErrs;
   };
@@ -184,6 +209,75 @@ const Members = () => {
                     onChange={onHanldeRadio}
                   />
                   {errs?.genders && <p>{errs?.genders}</p>}
+                </td>
+              </tr>
+
+              {/* interest */}
+              <tr>
+                <th scope="row">
+                  <label htmlFor="interests">Interests</label>
+                </th>
+                <td>
+                  <label htmlFor="sports">sports</label>
+                  <input
+                    type="checkbox"
+                    id="sport"
+                    name="interests"
+                    onChange={onHandleCheck}
+                  />
+
+                  <label htmlFor="sports">misic</label>
+                  <input
+                    type="checkbox"
+                    id="misuc"
+                    name="interests"
+                    onChange={onHandleCheck}
+                  />
+
+                  <label htmlFor="sports">game</label>
+                  <input
+                    type="checkbox"
+                    id="game"
+                    name="interests"
+                    onChange={onHandleCheck}
+                  />
+                  {errs?.interests && <p>{errs?.interests}</p>}
+                </td>
+              </tr>
+
+              {/* Education */}
+              <tr>
+                <th scope="row">
+                  <label htmlFor="edu">Education</label>
+                </th>
+                <td>
+                  <select name="edu" id="edu" onChange={onHandleChange}>
+                    <option value="">최종학력을 선택하세요.</option>
+                    <option value="elementary-school">초등학교 졸업</option>
+                    <option value="middle-school">중학교 졸업</option>
+                    <option value="high-school">고등학교 졸업</option>
+                    <option value="college">대학교 졸업</option>
+                  </select>
+
+                  {errs?.edu && <p>{errs?.edu}</p>}
+                </td>
+              </tr>
+
+              {/* comments */}
+              <tr>
+                <th scope="row">
+                  <label htmlFor="comments">comments</label>
+                </th>
+                <td>
+                  <textarea
+                    name="comments"
+                    id=""
+                    cols="30"
+                    rows="3"
+                    value={val.comments}
+                    onChange={onHandleChange}
+                  ></textarea>
+                  {errs?.comments && <p>{errs?.comments}</p>}
                 </td>
               </tr>
 
