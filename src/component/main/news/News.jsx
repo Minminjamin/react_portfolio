a260c329375271b0b716724db3c8a34e665bbb38
Minminjamin <0429el@gmail.com>
2023-10-16 09:25:20 +0900

    fix : localsotrage에 데이터 없을 경우 빈배열 반환 처리 (vercel 배포 오류 해결)

--- src/component/main/news/News.jsx
+++ src/component/main/news/News.jsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 import "./News.scss";
 
 const News = () => {
+  // 프로젝트가 처음 구동되면 무조건 Main페이지 이후 Community 페이지로 넘어가는 구조
+  // 해당 페이지에 있는 함수가 처음 구동될 시 로컬 저장소에 값이 없으므로 저장소 값이 없을 때 빈배열이 반환되는 구문을 추가
   const getLocalData = () => {
     const data = localStorage.getItem("post");
-    return JSON.parse(data);
+    if (data) return JSON.parse(data);
+    else return [];
   };
   const [post] = useState(getLocalData());
 
