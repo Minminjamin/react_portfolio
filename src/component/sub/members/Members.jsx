34516842a8ee2c5ca03896c8fa8732e08b7ce250
Minminjamin <0429el@gmail.com>
2023-10-16 09:38:07 +0900

    feature : 기본 input 요소 입력

--- src/component/sub/members/Members.jsx
+++ src/component/sub/members/Members.jsx
@@ -4,7 +4,44 @@ import Layout from "../../common/layout/Layout";
 const Members = () => {
   return (
     <Layout title={"Members"}>
-      <p>Members 페이지 입니다.</p>
+      <form>
+        <fieldset>
+          <legend>회원가입 폼 양식</legend>
+          <table>
+            <tbody border="1">
+              {/* userid */}
+              <tr>
+                <th scope="row">
+                  <label htmlFor="userid">user id</label>
+                </th>
+                <td>
+                  <input type="text" id="userid" name="userid" />
+                </td>
+              </tr>
+
+              {/* password */}
+              <tr>
+                <th scope="row">
+                  <label htmlFor="pwd1">password</label>
+                </th>
+                <td>
+                  <input type="password" id="pwd1" name="pwd1" />
+                </td>
+              </tr>
+
+              {/*re password */}
+              <tr>
+                <th scope="row">
+                  <label htmlFor="pwd1">re-password</label>
+                </th>
+                <td>
+                  <input type="password" id="pwd2" name="pwd2" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </fieldset>
+      </form>
     </Layout>
   );
 };
