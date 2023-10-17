9d7d92cad37b77c86983b51a0a3019f5de49b2e3
Minminjamin <0429el@gmail.com>
2023-10-17 10:37:49 +0900

    style : 햄버거 바 추가 및 module.scss를 scss로 변경

--- src/component/common/header/Header.jsx
+++ src/component/common/header/Header.jsx
@@ -1,48 +1,50 @@
 import clsx from "clsx";
 import React from "react";
-import styles from "./Header.module.scss";
+import "./Header.scss";
 import { Link, NavLink } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
 const Header = ({ isMain }) => {
-  console.log(isMain);
   return (
-    <header className={clsx(styles.header, isMain ? styles.main : styles.sub)}>
+    <header className="header">
       <h1>
         <Link to="/">LOGO</Link>
       </h1>
 
       <ul>
         <li>
-          <NavLink to="/department" activeClassName={styles.active}>
+          <NavLink to="/department" activeClassName="active">
             Department
           </NavLink>
         </li>
         <li>
-          <NavLink to="/community" activeClassName={styles.active}>
+          <NavLink to="/community" activeClassName="active">
             Community
           </NavLink>
         </li>
         <li>
-          <NavLink to="/gallery" activeClassName={styles.active}>
+          <NavLink to="/gallery" activeClassName="active">
             Gallery
           </NavLink>
         </li>
         <li>
-          <NavLink to="/youtube" activeClassName={styles.active}>
+          <NavLink to="/youtube" activeClassName="active">
             Youtube
           </NavLink>
         </li>
         <li>
-          <NavLink to="/members" activeClassName={styles.active}>
+          <NavLink to="/members" activeClassName="active">
             Members
           </NavLink>
         </li>
         <li>
-          <NavLink to="/contact" activeClassName={styles.active}>
+          <NavLink to="/contact" activeClassName="active">
             Contact
           </NavLink>
         </li>
       </ul>
+
+      <FaBars className="bars" fontSize={22} color={"#333"} />
     </header>
   );
 };
