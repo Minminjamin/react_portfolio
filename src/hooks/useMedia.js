53e3d30af1d2c039290ab47638c5a17eccc7b5df
Minminjamin <0429el@gmail.com>
2023-10-17 11:33:45 +0900

    refact : useMedia hook 오타 수정 및 laptaop시 type 초기화

--- src/hooks/useMedia.js
+++ src/hooks/useMedia.js
@@ -6,19 +6,21 @@ export const useMedia = (opt) => {
 
   // 해당 커스텀훅 호출시 특정 옵션이 전달되면 해당값으로 defaultOption 값을 덮어쓰기해서 합침
   const defaultOpt = {
-    mmobile: 640,
+    mobile: 640,
     tablet: 1000,
     laptop: 1400,
   };
-  const result = { ...defaultOpt, opt };
 
+  console.log(type);
+  const result = { ...defaultOpt, ...opt };
+  console.log(result);
   // 합쳐진 미디어 쿼리 수치값과 현재 브라우저 넓이값을 비교해서 Type의 문자값을 변경해주는 ㅎ마수
   const getClientWid = () => {
     let wid = window.innerWidth;
-
+    if (wid >= result.laptop) setType("");
     if (wid >= result.tablet && wid < result.laptop) setType("laptop");
-    if (wid >= result.mobile && wid < result.tablet) setType("tablet");
-    if (wid >= 0 && wid < result.mobile) setType("mobile");
+    else if (wid >= result.mobile && wid < result.tablet) setType("tablet");
+    else if (wid >= 0 && wid < result.mobile) setType("mobile");
   };
   useEffect(() => {
     // 마운트 시 실행하기 위해 일단 호출
