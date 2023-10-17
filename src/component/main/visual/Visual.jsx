981dd2174d21fc5e77ad1505b974a32656c2ab24
Minminjamin <0429el@gmail.com>
2023-10-17 16:36:31 +0900

    feature : vidsual에 youtube 데이터 출력

--- src/component/main/visual/Visual.jsx
+++ src/component/main/visual/Visual.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import "../visual/Visual.scss";
+import "./Visual.scss";
+import { useSelector } from "react-redux";
 
 const Visual = () => {
+  const { data } = useSelector((store) => store.youtube);
+
   return (
     <section className="visual">
       <h2>Visual</h2>
+      {data.map((item, idx) => {
+        if (idx >= 5) return;
+        return (
+          <article key={idx}>
+            <h2>{item.snippet.title}</h2>
+          </article>
+        );
+      })}
     </section>
   );
 };
