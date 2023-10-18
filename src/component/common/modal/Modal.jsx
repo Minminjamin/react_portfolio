45b13100542811de100e9321bd34c6c1fd0b4543
Minminjamin <0429el@gmail.com>
2023-10-18 16:41:22 +0900

    style : open 여부 및 fadeout 방향 변경

--- src/component/common/modal/Modal.jsx
+++ src/component/common/modal/Modal.jsx
@@ -1,24 +1,36 @@
 import React, { useEffect } from "react";
 import "../modal/Modal.scss";
 import { close } from "../../../redux/modalSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Modal = ({ children }) => {
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
-
-    return () => {
-      document.body.style.overflow = "auto";
-    };
-  }, []);
-
   const dispatch = useDispatch();
+  const { isOpen } = useSelector((state) => state.modal);
+
+  useEffect(() => {
+    isOpen
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "auto");
+  }, [isOpen]);
 
   return (
-    <aside className="modal">
-      <div className="con">{children}</div>
-      <span onClick={() => dispatch(close())}>close</span>
-    </aside>
+    <AnimatePresence>
+      {isOpen && (
+        <>
+          <motion.aside
+            className="modal"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <div className="con">{children}</div>
+            <span onClick={() => dispatch(close())}>close</span>
+          </motion.aside>
+        </>
+      )}
+    </AnimatePresence>
   );
 };
 
