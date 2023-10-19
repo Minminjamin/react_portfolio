1a6e7fb766cfe4edad573e2de4734a63d002054d
Minminjamin <0429el@gmail.com>
2023-10-19 09:37:08 +0900

    style : Layout 스타일 적용 및 module.scss를 scss로 변경

--- src/component/common/layout/Layout.jsx
+++ src/component/common/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../layout/Layout.module.scss";
+import "./Layout.scss";
 import clsx from "clsx";
 import { useEffect, useRef, useState } from "react";
 import useSplitText from "../../../hooks/useSplitText";
@@ -7,7 +7,7 @@ import useSplitText from "../../../hooks/useSplitText";
 const path = process.env.PUBLIC_URL;
 
 const Layout = ({ title, children, styleName }) => {
-  const [isOn, setIsOn] = useState(false);
+  // const [isOn, setIsOn] = useState(false);
 
   const frame = useRef(null);
   const tit = useRef(null);
@@ -16,22 +16,21 @@ const Layout = ({ title, children, styleName }) => {
 
   useEffect(() => {
     splitText(tit, 0.1, 1);
-    setTimeout(() => setIsOn(true), 300);
+    setTimeout(() => {
+      frame.current.classList.add("on");
+    }, 300);
+    // setTimeout(() => setIsOn(true), 300);
   }, []);
 
   return (
-    <section
-      ref={frame}
-      className={clsx(styles.layout, styleName, title, isOn ? styles.on : " ")}
-    >
+    <section ref={frame} className={`layout ${title}`}>
+      <h1 ref={tit}>{title}</h1>
+      <div className="bar"></div>
+
       <figure>
         <img src={`${path}/img/banner.jpg`} />
       </figure>
-
-      <div className={clsx(styles.content, styleName)}>
-        <h1 ref={tit}>{title}</h1>
-        {children}
-      </div>
+      {children}
     </section>
   );
 };
