7d8f526ca36e218d73f0710bed1b4673c1507b4b
Minminjamin <0429el@gmail.com>
2023-10-19 13:44:23 +0900

    style : youtube 썸네일 swiper 작업

--- src/component/main/visual/Visual.jsx
+++ src/component/main/visual/Visual.jsx
@@ -1,35 +1,57 @@
 import React from "react";
 import "./Visual.scss";
 import { useSelector } from "react-redux";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useState } from "react";
+import "swiper/css";
 
 const Visual = () => {
   const { data } = useSelector((store) => store.youtube);
 
+  const [index, setIndex] = useState();
+
   return (
     <section className="visual">
-      <div className="left">
-        <p>Video</p>
-        <h2>The whole world in one this page</h2>
-        <p>
-          Senectus et netus et malesuada. Nunc pulvinar sapien et ligula
-          ullamcorper malesuada proin.
-        </p>
-      </div>
+      <div className="titBox">
+        <ul>
+          {data.map((item, idx) => {
+            if (idx >= 6) return null;
+            return (
+              <li key={idx} className={idx === index ? "on" : ""}>
+                <h3>{item.snippet.title}</h3>
 
-      <div className="right">
+                <button>View Detail</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={50}
+        loop={true}
+        centeredSlides={true}
+        // swiper loop 기능을 적용하는 순간 실제 연결되어 있는 패널갯수보다 동적으로 패널이 생성되면서 일반적인 방법으로는 활성화 패널이 순서값을 구할 수 없기 때문에 아래와 같은 방법으로 순서를 구함
+        onSlideChange={(el) => setIndex(el.realIndex)} //item과 중복을 피하기 위해 el 사용
+      >
         {data.map((item, idx) => {
-          if (idx >= 4) return;
+          if (idx >= 6) return null;
           return (
-            <article key={idx}>
-              <img
-                src={item.snippet.thumbnails.standard.url}
-                alt={item.title}
-                // onClick={() => setIndex(idx)}
-              />
-            </article>
+            <SwiperSlide key={idx}>
+              <div className="pic">
+                <img
+                  src={item.snippet.thumbnails.maxres.url}
+                  alt={item.title}
+                />
+                <img
+                  src={item.snippet.thumbnails.maxres.url}
+                  alt={item.title}
+                />
+              </div>
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
     </section>
   );
 };
