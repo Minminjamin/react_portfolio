efb27834da9563752d09387ee28e1fabab30d29b
Minminjamin <0429el@gmail.com>
2023-10-22 21:27:03 +0900

    fix & style : 초기 로딩 시 첫 유튜브 제목이 보이지 않는 문제 해결

--- src/component/main/visual/Visual.jsx
+++ src/component/main/visual/Visual.jsx
@@ -10,7 +10,7 @@ const Visual = () => {
   // const { data } = useSelector((store) => store.youtube);
   const { data, isSuccess } = useYoutubeQuery();
 
-  const [index, setIndex] = useState();
+  const [index, setIndex] = useState(0);
 
   const history = useHistory();
 
@@ -52,7 +52,7 @@ const Visual = () => {
         </div>
         <Swiper
           slidesPerView={1}
-          spaceBetween={0}
+          spaceBetween={50}
           loop={true}
           centeredSlides={true}
           // swiper loop 기능을 적용하는 순간 실제 연결되어 있는 패널갯수보다 동적으로 패널이 생성되면서 일반적인 방법으로는 활성화 패널이 순서값을 구할 수 없기 때문에 아래와 같은 방법으로 순서를 구함
