b948c52a3d986bdb0d059eb3abe56d37923123f1
Minminjamin <0429el@gmail.com>
2023-10-23 10:23:37 +0900

    feature ; 전역 데이터 컴포넌트 커스텀훅 생성

---
+++ src/hooks/useGlobalContext.js
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from "react";
+
+// 컴포넌트 선언을 위한 camel case 사용
+export const GlobalContext = createContext();
+
+// provider이므로 camel case 사용
+export function GlobalProvider({ children }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <GlobalContext.Provider value={(menuOpen, setMenuOpen)}>
+      {children}
+    </GlobalContext.Provider>
+  );
+}
+
+export function useGlobalData() {
+  const globalContext = useContext(GlobalContext);
+
+  return globalContext;
+}
